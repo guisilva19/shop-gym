3fb280ca99dba60ba7d54af66fc613331640f328
unknown <2003silvagui@gmail.com>
2024-04-18 15:23:41 -0300

    feat: header responsive

---
+++ src/components/dropdown/Dropdown.tsx
@@ -0,0 +1,13 @@
+import { HiMenuAlt3 } from "react-icons/hi";
+
+export default function Dropdown() {
+  return (
+    <>
+      <div className="flex h-full items-center">
+        <button className="">
+          <HiMenuAlt3 size={40} color="#fff" />
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,30 +1,42 @@
 import Image from "next/image";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import logo from "../../assets/logo.png";
+import Dropdown from "../dropdown/Dropdown";
 
 export default function Header() {
   const navList = ["Deals", "Dumbbells", "Kettlebells", "Barbells & Plates "];
 
   return (
     <>
-      <header className="w-screen h-20 absolute inset-0 z-20 flex justify-between items-center px-28 animate-fade-down animate-once">
-        <div className="w-52">
-          <Image src={logo} alt="logo" width={150} height={150} />
+      <header className="w-screen h-20 absolute inset-0 z-20 flex justify-between items-center px-5 md:px-28 animate-fade-down animate-once">
+        <div className="w-max">
+          <Image
+            src={logo}
+            alt="logo"
+            width={100}
+            height={100}
+            className="md:w-[150px] "
+          />
         </div>
-        <nav className="mx-auto flex ">
+        <nav className="mx-auto hidden lg:flex">
           <ul className="flex justify-between w-[530px]">
             {navList.map((item, index) => (
               <li key={index}>
-                <p className="text-white cursor-pointer hover:scale-110 duration-500">{item}</p>
+                <p className="text-white cursor-pointer hover:scale-110 duration-500">
+                  {item}
+                </p>
               </li>
             ))}
           </ul>
         </nav>
-        <div className="w-52">
+        <div className="w-52 hidden lg:flex">
           <button className="px-10 py-4 rounded-full bg-white font-bold flex items-center gap-2 text-lg hover:animate-jump animate-once animate-duration-500">
             <MdOutlineShoppingCart size={25} />
             Shop Now
           </button>
+        </div>
+        <div className="flex lg:hidden items-center justify-center">
+          <Dropdown />
         </div>
       </header>
     </>
