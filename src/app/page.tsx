7d47371a998a759ae8946b2406d2342bc27d0b8e
unknown <2003silvagui@gmail.com>
2024-04-17 23:39:02 -0300

    feat: page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/header/header";
+import Hero from "@/components/hero/hero";
+
 export default function Home() {
   return (
     <>
-      <h1></h1>
+      <Header />
+      <Hero />
     </>
   );
 }
